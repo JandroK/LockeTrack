842fcdf3c98564437027daeee3e714e30133e93c
JandroK <61042882+JandroK@users.noreply.github.com>
2021-12-15 00:11:34 +0100

    Update rutes.dart

--- locketrack/lib/screens/rutes.dart
+++ locketrack/lib/screens/rutes.dart
@@ -1,18 +1,38 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class RuteScreen extends StatelessWidget {
+class RuteScreen extends StatefulWidget {
   RuteScreen({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<RuteScreen> createState() => _RuteScreenState();
+}
+
+class _RuteScreenState extends State<RuteScreen> {
   final db = FirebaseFirestore.instance;
+  List<String> routes = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    routes = [for (int i = 0; i < 10; i++) "Hola"];
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(12.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           const Center(
               child: Text("Kanto",
@@ -29,7 +49,39 @@ class RuteScreen extends StatelessWidget {
                   fit: BoxFit.fitHeight,
                   image: AssetImage("assets/kanto_map.jpg")),
             ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.green, width: 5),
+                borderRadius: const BorderRadius.all(Radius.circular(15))),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text("Route Name"),
+                    Text("Pokémon"),
+                    Text("Status")
+                  ],
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.orange),
+                  child: const Icon(Icons.add),
+                )
+              ],
+            ),
           )
+          // ListView.builder(itemBuilder: (BuildContext context, int index) {
+          //   return Container(
+          //     //decoration: const BoxDecoration(
+          //     //shape: BoxShape.rectangle, color: Colors.green),
+          //     child: Text(
+          //       routes[index],
+          //     ),
+          //   );
+          // })
         ],
       ),
     );
