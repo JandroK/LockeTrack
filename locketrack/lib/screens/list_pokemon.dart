422d1c83dd79b93b07cc71cc4d34dfd049594662
Alejandro Moreno <61042882+JandroK@users.noreply.github.com>
2021-12-22 22:17:36 +0100

    New Style Container pokedex

--- locketrack/lib/screens/list_pokemon.dart
+++ locketrack/lib/screens/list_pokemon.dart
@@ -13,10 +13,39 @@ class Pokedex extends StatefulWidget {
 
 class _PokedexState extends State<Pokedex> {
   final db = FirebaseFirestore.instance;
-
+  bool cath = false;
   List<String> routes = [];
 
-  void drawPokemons() async {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  void generatePokemons(CollectionReference<Map<String, dynamic>> collection,
+      List<String> pokedex) {
+    collection.get().then((value) {
+      if (value.size == 0) {
+        int i = 0;
+        pokedex.forEach((element) async {
+          await collection.add({
+            'name': element,
+            'number_dex': "",
+            'type1': "",
+            'type2': "",
+          });
+        });
+      }
+    }).then((value) => loadPokemons());
+  }
+
+  void loadPokemons() async {
     await db.collection("pokemons").get().then((querySnapshot) {
       querySnapshot.docs.forEach((result) {
         routes.add(result.id);
@@ -24,7 +53,7 @@ class _PokedexState extends State<Pokedex> {
     });
     setState(() {
       // Update your UI with the desired changes.
-      print("Load Route info");
+      print("Load pokemons id");
     });
   }
 
@@ -35,77 +64,93 @@ class _PokedexState extends State<Pokedex> {
         title: const Text("Pokédex"),
       ),
       body: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Center(
-            child: ElevatedButton(
-              child: const Text("Back"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ),
           Card(
-            child: TextButton(
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.white)),
+            margin: const EdgeInsets.all(10),
+            child: ElevatedButton(
               onPressed: () {},
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: const [
-                            Text("#NUM"),
-                            SizedBox(height: 10),
-                            Text("Pokémon: Name "),
-                          ],
-                        ),
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            ContainerType("Type"),
-                            ContainerType("Type"),
-                          ],
-                        )
-                      ],
+              // Probar a hacer un degradado segun los tipos
+              style: ElevatedButton.styleFrom(primary: Colors.black38),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              const Text("#001"),
+                              const SizedBox(width: 20),
+                              const Text("Bulbasaur"),
+                              Expanded(
+                                child: IconButton(
+                                  alignment: Alignment.centerRight,
+                                  padding: const EdgeInsets.all(0),
+                                  constraints:
+                                      const BoxConstraints(maxHeight: 34),
+                                  icon: Icon((cath)
+                                      ? Icons.star_border_rounded
+                                      : Icons.star_rate_rounded),
+                                  onPressed: () {
+                                    setState(() {
+                                      cath = !cath;
+                                    });
+                                  },
+                                ),
+                              )
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            children: [
+                              ContainerType("Type"),
+                              ContainerType("Type"),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(left: 10),
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.orange,
+                    Container(
+                      margin: const EdgeInsets.only(left: 10),
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.orange,
+                      ),
+                      child: Transform.rotate(
+                          angle: 180 * math.pi / 180,
+                          child: const Icon(Icons.catching_pokemon_rounded,
+                              size: 60)),
                     ),
-                    child: Transform.rotate(
-                        angle: 180 * math.pi / 180,
-                        child: const Icon(Icons.catching_pokemon_rounded,
-                            size: 70)),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-            margin: EdgeInsets.all(20),
-            elevation: 12,
           )
         ],
       ),
     );
   }
 
-  Container ContainerType(String type) {
-    return Container(
-      child: Text(type),
-      margin: const EdgeInsets.only(left: 10),
-      decoration: BoxDecoration(
-        shape: BoxShape.rectangle,
-        border: Border.all(color: Colors.white, width: 1),
+  Expanded ContainerType(String type) {
+    return Expanded(
+      child: Container(
+        child: Text(
+          type,
+          textAlign: TextAlign.center,
+        ),
+        padding: const EdgeInsets.all(4),
+        margin: const EdgeInsets.symmetric(horizontal: 4),
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(6)),
+          border: Border.all(color: Colors.white, width: 1),
+        ),
       ),
     );
   }
