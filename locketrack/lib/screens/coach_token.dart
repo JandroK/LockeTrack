eb2d71dc305285ac90d9600892625db5d5c0ccc6
Denisdrk6 <denisdsuniversidad@hotmail.com>
2021-12-22 12:31:59 +0100

    medals and heart functionallity

--- locketrack/lib/screens/coach_token.dart
+++ locketrack/lib/screens/coach_token.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:locketrack/screens/list_pokemon.dart';
+import 'package:drop_shadow_image/drop_shadow_image.dart';
 
 class CoachToken extends StatefulWidget {
   final String docID;
@@ -76,103 +77,85 @@ class _CoachTokenState extends State<CoachToken> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Row(
-            children: [
-              for (int i = 0; i < medals.length / 2; i++)
-                Expanded(
-                  child: MedalSnapshot(
-                    db: db,
-                    path: medals[i],
-                    index: i,
-                    medalCount: medalCount,
-                  ),
-                ),
-            ],
-          ),
-          Row(
-            children: [
-              for (int i = 4; i < medals.length; i++)
-                Expanded(
-                  child: MedalSnapshot(
-                      db: db,
-                      path: medals[i],
-                      index: i,
-                      medalCount: medalCount),
-                ),
-            ],
-          ),
-          Row(
-            children: [
-              for (int i = 0; i < 5; i++)
-                Expanded(
-                  child: (i > lives - 1)
-                      ? const ColorFiltered(
-                          colorFilter: ColorFilter.mode(
-                              Colors.black38, BlendMode.modulate),
-                          child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Image(
-                              image: AssetImage("assets/heart.png"),
-                            ),
-                          ),
-                        )
-                      : Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: const Image(
-                            image: AssetImage("assets/heart.png"),
-                          ),
-                        ),
-                ),
-            ],
-          ),
-          Row(
-            children: [
-              for (int i = 5; i < 10; i++)
-                Expanded(
-                  child: (i > lives - 1)
-                      ? const ColorFiltered(
-                          colorFilter: ColorFilter.mode(
-                              Colors.black38, BlendMode.modulate),
-                          child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Image(
-                              image: AssetImage("assets/heart.png"),
-                            ),
-                          ),
-                        )
-                      : Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: const Image(
-                            image: AssetImage("assets/heart.png"),
-                          ),
-                        ),
-                ),
-            ],
-          ),
+          MedalRow(0, medals.length ~/ 2),
+          MedalRow(medals.length ~/ 2, medals.length),
+          LiveRow(0, 5),
+          LiveRow(5, 10),
         ],
       ),
     );
   }
+
+  // ignore: non_constant_identifier_names
+  Row LiveRow(int init, int length) {
+    return Row(
+      children: [
+        for (int i = init; i < length; i++)
+          Expanded(
+              child: HeartSnapshot(
+            db: db,
+            index: i,
+            lives: lives,
+            onWidgetUpdate: () {
+              setState(() {
+                db.update({"lives": i + 1});
+                lives = i + 1;
+              });
+            },
+          )),
+      ],
+    );
+  }
+
+  // ignore: non_constant_identifier_names
+  Row MedalRow(int init, int length) {
+    return Row(
+      children: [
+        for (int i = init; i < length; i++)
+          Expanded(
+            child: MedalSnapshot(
+              db: db,
+              path: medals[i],
+              index: i,
+              medalCount: medalCount,
+              onMedalsChanged: () {
+                setState(() {
+                  db.update({"medals": i + 1});
+                  medalCount = i + 1;
+                });
+              },
+            ),
+          ),
+      ],
+    );
+  }
 }
 
-class MedalSnapshot extends StatelessWidget {
+class MedalSnapshot extends StatefulWidget {
+  final VoidCallback onMedalsChanged;
   final String path;
   final int index;
-  final int medalCount;
-  const MedalSnapshot({
+  late int medalCount;
+  MedalSnapshot({
     required this.path,
     required this.index,
     required this.medalCount,
     required this.db,
+    required this.onMedalsChanged,
     Key? key,
   }) : super(key: key);
 
   final DocumentReference<Map<String, dynamic>> db;
 
   @override
+  State<MedalSnapshot> createState() => _MedalSnapshotState();
+}
+
+class _MedalSnapshotState extends State<MedalSnapshot> {
+  @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: db.collection("medals").doc(path).snapshots(),
+      stream: widget.db.collection("medals").doc(widget.path).snapshots(),
       builder: (
         BuildContext context,
         AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> snapshot,
@@ -185,21 +168,72 @@ class MedalSnapshot extends StatelessWidget {
         }
         final doc = snapshot.data!.data();
         if (doc != null) {
-          return (index > medalCount - 1)
-              ? ColorFiltered(
-                  colorFilter:
-                      ColorFilter.mode(Colors.black38, BlendMode.modulate),
-                  child: Image(
-                    image: AssetImage("assets/medals/" + doc["name"] + ".png"),
-                  ),
-                )
-              : Image(
-                  image: AssetImage("assets/medals/" + doc["name"] + ".png"),
-                );
+          return IconButton(
+            icon: (widget.index > widget.medalCount - 1)
+                ? ColorFiltered(
+                    colorFilter: const ColorFilter.mode(
+                        Colors.black38, BlendMode.modulate),
+                    child: Image.asset("assets/medals/" + doc["name"] + ".png"),
+                  )
+                : Stack(children: [
+                    ColorFiltered(
+                      colorFilter: const ColorFilter.mode(
+                          Colors.black, BlendMode.modulate),
+                      child: DropShadowImage(
+                        image: Image.asset(
+                            "assets/medals/" + doc["name"] + ".png"),
+                        blurRadius: 5.0,
+                        offset: Offset(3.0, 3.0),
+                      ),
+                    ),
+                    Image.asset("assets/medals/" + doc["name"] + ".png"),
+                  ]),
+            onPressed: widget.onMedalsChanged,
+          );
         } else {
           return const Center(child: Text("doc is null!"));
         }
       },
+    );
+  }
+}
+
+class HeartSnapshot extends StatefulWidget {
+  final VoidCallback onWidgetUpdate;
+  final int index;
+  late int lives;
+  HeartSnapshot({
+    required this.index,
+    required this.lives,
+    required this.db,
+    required this.onWidgetUpdate,
+    Key? key,
+  }) : super(key: key);
+
+  final DocumentReference<Map<String, dynamic>> db;
+
+  @override
+  State<HeartSnapshot> createState() => _HeartSnapshotState();
+}
+
+class _HeartSnapshotState extends State<HeartSnapshot> {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: (widget.index > widget.lives - 1)
+          ? ColorFiltered(
+              colorFilter: ColorFilter.mode(Colors.black38, BlendMode.modulate),
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Image.asset("assets/heart.png"),
+              ),
+            )
+          : Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Image.asset("assets/heart.png"),
+            ),
+      onPressed: widget.onWidgetUpdate,
+      iconSize: 50,
     );
   }
 }
